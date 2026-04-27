a8ac1a952bd4ac59008475cd37068beef1317d78
tania <taniauniaokoth@gmail.com>
2026-04-27 22:47:10 +0300

    Implement date logic and name retrieval in getName function

--- code.js
+++ code.js
@@ -19,4 +19,18 @@ function getName() {
   let male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   let female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
-  
+  // date logic
+  let date = new Date(year, month - 1, day);
+  let d = date.getDay();
+
+  let name;
+
+  if (gender === "male") {
+    name = male[d];
+  } else {
+    name = female[d];
+  }
+
+  // output
+  document.getElementById("result").innerHTML = "Your Akan name is " + name;
+}
