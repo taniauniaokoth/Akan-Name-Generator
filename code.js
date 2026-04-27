403312c9a580f8c32292e3b6d6093b091165de9b
tania <taniauniaokoth@gmail.com>
2026-04-27 22:44:00 +0300

    Add getName function to retrieve user input values

---
+++ code.js
@@ -0,0 +1,7 @@
+function getName() {
+  let day = document.getElementById("day").value;
+  let month = document.getElementById("month").value;
+  let year = document.getElementById("year").value;
+  let gender = document.getElementById("gender").value;
+
+  
